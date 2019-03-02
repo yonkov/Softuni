3ceb796a0c571c03021b4c9ed7c1741d90046442
Yonkov <yonkov.atanas@gmail.com>
2019-03-02 17:02:25 +0200

    Update React JS project

--- JS Web/ReactJS Fundamentals/final project/server/routes/feed.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const feedController = require('../controllers/feed');
-const isAuth = require('../middleware/is-auth');
-
-router.get('/movies', isAuth, feedController.getMovies);
-router.post('/movie/create', isAuth, feedController.createMovie);
-
-module.exports = router;
